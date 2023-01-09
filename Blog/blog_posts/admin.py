70d4bb2d39b591d7160fbba78767e999f0ba8876
Harry <harryfry11@gmail.com>
2023-01-09 21:29:14 +0000

    adding categories to blog posts

--- Blog/blog_posts/admin.py
+++ Blog/blog_posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Category
 
 admin.site.register(Post)
+
+admin.site.register(Category)
--- Blog/blog_posts/models.py
+++ Blog/blog_posts/models.py
@@ -1,8 +1,20 @@
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    title= models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return(self.title)
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -11,3 +23,6 @@ class Post(models.Model):
 
     class Meta:
         ordering = ('-created_at',)
+
+    def __str__(self):
+        return(self.title)
